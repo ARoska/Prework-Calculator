d4c651358be8cab6dd656d33bb9dcea775e7089b
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-18 13:26:47 -0700

    Comments code.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,11 +10,13 @@ namespace ConsoleApp1
             int valueTwo;
             string command;
 
+            //Asks user for input of which command they would like to run
             Console.WriteLine("Which function would you like to use? (Choices are: Add, Subtract, Multiply, Divide)");
             command = Console.ReadLine();
             command = command.ToUpper();
             Console.Clear();
 
+            //Runs the command the user chose
             if(command == "ADD" || command == "AD" || command == "PLUS")
             {
                 Console.WriteLine("ADD");
@@ -67,26 +69,36 @@ namespace ConsoleApp1
                 Console.WriteLine($"The quotent of {valueTwo} and {valueOne} is {answer}.");
             }
 
+            //If command is not recognized
+            else
+            {
+                Console.WriteLine("Not a valid command.");
+            }
+
             Console.WriteLine();
             Console.WriteLine("Press enter to close.");
             Console.ReadLine();
         }
 
+        //Add helper method
         public static int Add(int valueOne, int valueTwo)
         {
             return valueOne + valueTwo;
         }
 
+        //Subtract helper method
         public static int Subtract(int valueOne, int valueTwo)
         {
             return valueOne - valueTwo;
         }
 
+        //Multiply helper method
         public static int Multiply(int valueOne, int valueTwo)
         {
             return valueOne * valueTwo;
         }
 
+        //Divide helper method
         public static double Divide(double valueOne, double valueTwo)
         {
             return valueTwo / valueOne;
